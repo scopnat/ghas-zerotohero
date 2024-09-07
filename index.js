d391b1fec865bd3050bf29845a6d96479d622f46
Scopnat Inc <98498191+scopnat@users.noreply.github.com>
2024-09-07 01:17:22 +0000

    add new code for codeql

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// Non functional code....
+app.get('/some/path', function(req, res) {
+  let url = req.param('url'),
+      host = urlLib.parse(url).host;
+  let regex = /^((www|beta).)?example.com/;
+  if (host.match(regex)) {
+      res.redirect(url);
+  }
+});
